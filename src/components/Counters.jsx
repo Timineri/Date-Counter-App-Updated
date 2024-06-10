2ccf2bb9d7553411e8f770d877dff9931a598db9
Timineri Okoko <tonyeokoko@outlook.com>
2024-06-11 00:51:19 +0100

    updated the date-counter challenge with different features

--- src/components/Counters.jsx
+++ src/components/Counters.jsx
@@ -5,17 +5,22 @@ export default function Counter() {
   const [step, setStep] = useState(1);
   const [count, setCount] = useState(0);
 
-  function handleIncrementStep() {
-    setStep((step) => step + 1);
+  function handleIncrementStep(e) {
+    setStep(Number(e.target.value));
   }
   function handleIncrementCount() {
     setCount((count) => count + step);
   }
-  function handleDecrementStep() {
-    setStep((step) => step - 1);
-  }
   function handleDecrementCount() {
     setCount((count) => count - step);
+  }
+  function countChange(e) {
+    setCount(Number(e.target.value));
+  }
+
+  function handleReset() {
+    setCount(0);
+    setStep(1);
   }
   const date = new Date();
   date.setDate(date.getDate() + count);
@@ -23,13 +28,23 @@ export default function Counter() {
   return (
     <div>
       <div>
-        <button onClick={handleDecrementStep}>-</button>
+        <input
+          type="range"
+          min="1"
+          max="7"
+          value={step}
+          onChange={handleIncrementStep}
+        />
+        <span>{step}</span>
+        {/* <button onClick={handleDecrementStep}>-</button>
         <span>Step: {step}</span>
-        <button onClick={handleIncrementStep}>+</button>
+        <button onClick={handleIncrementStep}>+</button> */}
       </div>
       <div>
         <button onClick={handleDecrementCount}>-</button>
-        <span>Count: {count}</span>
+        <span>
+          <input type="text" value={count} onChange={countChange} />
+        </span>
         <button onClick={handleIncrementCount}>+</button>
       </div>
       <span>
@@ -40,6 +55,10 @@ export default function Counter() {
           : `${Math.abs(count)} days ago was `}
       </span>
       <span>{date.toDateString()}</span>
+
+      <div>
+        <button onClick={handleReset}>Reset</button>
+      </div>
     </div>
   );
 }
